2ecf2a404517da43e5ec83a303aef7169fa2faec
gabrnavarro <gabrnavarro@gmail.com>
2016-07-12 17:11:42 +0800

    Add project files.

---
+++ ProjectorCSharp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProjectorCSharp.Startup))]
+namespace ProjectorCSharp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
